78d5b668bec155430b64e164b4c929ccb83412da
romanovamar <43072318+romanovamar@users.noreply.github.com>
2018-09-18 17:27:45 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,7 +2,7 @@
 unique=lambda e: sorted(set(e))
 transposeDict=lambda d: {d[key]:key for key in d}
 mex=lambda e,i=1: i if i not in e else mex(e,i=i+1)             
-frequencyDict=lambda s: {i:d.count(i) for i in d}   
+frequencyDict=lambda s: {i:s.count(i) for i in s}   
 
 
 		
