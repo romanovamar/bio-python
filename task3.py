7bfb8e0af26bee8df36799d5a793c691f22a0803
romanovamar <43072318+romanovamar@users.noreply.github.com>
2018-10-07 22:30:29 +0200

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,40 @@
+from functools import reduce
+
+
+def squares(a):
+    for i in a:
+        yield i ** 2
+
+
+def repeatntimes(elems, n):
+    for i in range(n):
+        for f in elems:
+            yield f
+
+
+def evens(x):
+    even = x if x % 2 == 0 else x + 1
+    while True:
+        yield even
+        even += 2
+
+
+def digitsumdiv(p):
+    f = p
+    while True:
+        if sum([int(i) for i in str(p)]) % f == 0:
+            yield int(p)
+        p += 1
+
+
+def extractnumbers(s):
+    return filter(lambda x: x.isdigit(), s)
+
+
+def changecase(s):
+    return map(lambda x: x.lower() if x.isupper() else x.upper(), s)
+
+
+def productif(elems, conds):
+    conds = iter(conds)
+    return reduce(lambda x, y: x * y, filter(lambda f: next(conds), elems), 1)
