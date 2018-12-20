cc3dac5d8a78b6e1ffe734856db131be3ccbe4e5
romanovamar <43072318+romanovamar@users.noreply.github.com>
2018-12-20 23:38:52 +0300

    Update task9.py

--- task9.py
+++ task9.py
@@ -19,6 +19,7 @@ class FSItem(object):
                         raise FileSystemError if item does not exist
                         raise FileSystemError if item "newname" already exists '''
         try:
+            self.name = newname
             newname = os.path.join(self.cur_dir, newname)
             os.rename(self.path, newname)
         except:
